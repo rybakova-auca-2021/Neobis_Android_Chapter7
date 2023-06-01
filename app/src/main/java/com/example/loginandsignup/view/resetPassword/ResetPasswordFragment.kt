1c9cc4caf68c90b1fe693b9b1c8f44a02a55973c
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-01 17:40:17 +0600

    reset password - logic added

--- app/src/main/java/com/example/loginandsignup/view/resetPassword/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/loginandsignup/view/resetPassword/ResetPasswordFragment.kt
@@ -1,10 +1,15 @@
 package com.example.loginandsignup.view.resetPassword
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import com.example.loginandsignup.R
 import com.example.loginandsignup.databinding.FragmentResetPasswordBinding
@@ -23,14 +28,68 @@ class ResetPasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigation()
+        setupPasswordValidation()
     }
 
-    fun navigation() {
+    private fun navigation() {
         binding.buttonBack.setOnClickListener {
             findNavController().navigate(R.id.action_resetPasswordFragment_to_resetSendToEmailFragment)
         }
         binding.loginButton2.setOnClickListener{
             findNavController().navigate(R.id.action_resetPasswordFragment_to_loginFragment2)
         }
+        binding.isPasswordVisible.setOnClickListener {
+            togglePasswordVisibilityCreate()
+        }
+        binding.isPasswordVisible2.setOnClickListener {
+            togglePasswordVisibilityRepeat()
+        }
+    }
+
+    private fun setupPasswordValidation() {
+        binding.createPassword.addTextChangedListener {
+            val password = it.toString()
+
+            val hasUpperCase = password.matches(".*[A-Z].*".toRegex())
+            val hasNumber = password.matches(".*\\d.*".toRegex())
+            val hasSpecialSymbol = password.matches(".*[!@#$%^&*(),.?\":{}|<>].*".toRegex())
+            val isPasswordMatch = password == binding.repeatPassword.text.toString()
+
+            val validColor = ContextCompat.getColor(requireContext(), R.color.validColor)
+
+            binding.isLetterBig.setTextColor(if (hasUpperCase) validColor else Color.RED)
+            binding.pointBigLetter.setColorFilter(if (hasUpperCase) validColor else Color.RED)
+
+            binding.areThereNumbers.setTextColor(if (hasNumber) validColor else Color.RED)
+            binding.pointNumbers.setColorFilter(if (hasNumber) validColor else Color.RED)
+
+            binding.specialSymbols.setTextColor(if (hasSpecialSymbol) validColor else Color.RED)
+            binding.pointSymbols.setColorFilter(if (hasSpecialSymbol) validColor else Color.RED)
+
+            binding.isUniquePassword.setTextColor(if (isPasswordMatch) validColor else Color.RED)
+            binding.pointPassword.setColorFilter(if (isPasswordMatch) validColor else Color.RED)
+
+            val isValid = hasUpperCase && hasNumber && hasSpecialSymbol && isPasswordMatch
+
+            binding.loginButton2.isEnabled = isValid
+
+            // Change button color based on validity
+            val buttonColor = if (isValid) R.color.invalidButtonColor else R.color.validButtonColor
+            val buttonColorRes = ContextCompat.getColor(requireContext(), buttonColor)
+            binding.loginButton2.setBackgroundColor(buttonColorRes)
+        }
+    }
+
+    private fun togglePasswordVisibilityCreate() {
+        val isPasswordVisible = binding.createPassword.transformationMethod == HideReturnsTransformationMethod.getInstance()
+        val newTransformationMethod = if (isPasswordVisible) PasswordTransformationMethod.getInstance() else HideReturnsTransformationMethod.getInstance()
+        binding.createPassword.transformationMethod = newTransformationMethod
+        binding.createPassword.setSelection(binding.createPassword.text?.length ?: 0)
+    }
+    private fun togglePasswordVisibilityRepeat() {
+        val isPasswordVisible = binding.repeatPassword.transformationMethod == HideReturnsTransformationMethod.getInstance()
+        val newTransformationMethod = if (isPasswordVisible) PasswordTransformationMethod.getInstance() else HideReturnsTransformationMethod.getInstance()
+        binding.repeatPassword.transformationMethod = newTransformationMethod
+        binding.repeatPassword.setSelection(binding.createPassword.text?.length ?: 0)
     }
 }
--- app/src/main/java/com/example/loginandsignup/view/resetPassword/ResetSendToEmailFragment.kt
+++ app/src/main/java/com/example/loginandsignup/view/resetPassword/ResetSendToEmailFragment.kt
@@ -1,9 +1,14 @@
 package com.example.loginandsignup.view.resetPassword
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.loginandsignup.R
@@ -23,14 +28,45 @@ class ResetSendToEmailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigation()
+        changeButtonColor()
     }
 
-    fun navigation() {
+    private fun navigation() {
         binding.buttonBack.setOnClickListener {
             findNavController().navigate(R.id.action_resetSendToEmailFragment_to_loginFragment2)
         }
         binding.loginButton.setOnClickListener {
-            //TODO - open dialog and send message to mail
+            showCustomDialog()
         }
+    }
+    private fun changeButtonColor() {
+        binding.emailButton.addTextChangedListener { text ->
+            val email = text?.toString() ?: ""
+            val isValidEmail = isValidEmail(email)
+            val buttonColor = if (isValidEmail) R.color.invalidButtonColor else R.color.validButtonColor
+            binding.loginButton.isEnabled = isValidEmail
+            binding.loginButton.setBackgroundColor(requireContext().getColor(buttonColor))
+        }
+    }
+    private fun isValidEmail(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private fun showCustomDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_mail, null)
+        val dialogButton = dialogView.findViewById<Button>(R.id.dialogButton)
+
+        val builder = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setCancelable(true)
+
+        val alertDialog = builder.create()
+
+        dialogButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
     }
 }
