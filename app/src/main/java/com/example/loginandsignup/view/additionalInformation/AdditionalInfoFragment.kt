3497ac3239453be1e6576045fdf2898a48262611
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-02 17:13:31 +0600

    additional information fragment - ui fixed and added logic

--- app/src/main/java/com/example/loginandsignup/view/additionalInformation/AdditionalInfoFragment.kt
+++ app/src/main/java/com/example/loginandsignup/view/additionalInformation/AdditionalInfoFragment.kt
@@ -1,10 +1,13 @@
 package com.example.loginandsignup.view.additionalInformation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.loginandsignup.R
 import com.example.loginandsignup.databinding.FragmentAdditionalInfoBinding
@@ -22,9 +25,34 @@ class AdditionalInfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigation()
+        validateInputFields()
     }
 
-    fun navigation() {
+    private fun isEmailValid(email: String): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(email).matches()
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun validateInputFields() {
+        val name = binding.inputRegNameProfile.text?.toString() ?: ""
+        val surname = binding.inputRegSurnameProfile.text?.toString() ?: ""
+        val birthDate = binding.inputRegBirthProfile.text?.toString() ?: ""
+        val email = binding.inputEmailProfile.text?.toString() ?: ""
+
+        val isValid = name.isNotEmpty() && surname.isNotEmpty() && birthDate.isNotEmpty() && isEmailValid(email)
+
+        // Change button color based on validity
+        if (isValid) {
+            binding.registration.setBackgroundColor(R.color.validButtonColor)
+            binding.registration.isEnabled = true
+        } else {
+            binding.registration.setBackgroundColor(R.color.invalidButtonColor)
+            binding.registration.isEnabled = false
+        }
+    }
+
+    private fun navigation() {
         binding.registration.setOnClickListener {
             findNavController().navigate(R.id.action_additionalInfoFragment2_to_createPasswordFragment2)
         }
