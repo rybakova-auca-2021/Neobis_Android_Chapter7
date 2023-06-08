85a71fb37c2b9fe3197bf03e6c7639b1605054fa
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-06-08 21:35:48 +0600

    fixed ui

--- app/src/main/java/com/example/loginandsignup/model/PersonalInfoRequest.kt
+++ app/src/main/java/com/example/loginandsignup/model/PersonalInfoRequest.kt
@@ -3,7 +3,6 @@ package com.example.loginandsignup.model
 data class PersonalInfoRequest(
     val first_name: String,
     val last_name: String,
-    val birth_date: Int,
-    val email: String
-    // Include other necessary fields
+    val birth_date: String,
+    var email: String
 )
